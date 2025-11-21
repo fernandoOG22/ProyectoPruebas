41598f1a207d11b5972a7bb5e2795b4e4b40f294
ErickDH25 <l22170682@culiacan.tecnm.mx>
2025-11-20 22:28:33 -0700

    bambusa

--- src/test/java/AppTests.java
+++ src/test/java/AppTests.java
@@ -9,14 +9,10 @@ import org.junit.Test;
 
 public class AppTests {
     @Test
-    public void testAdd_WhenLeftListContainsNegativeNumber_ShouldThrowException() {
-        // 1. Preparar (Arrange)
-        // Introducimos un -5 en la lista izquierda
+    public void testCuandoLeftListContieneElementoNegativo_DeberiaArrojarException() {
         List<Integer> left = Arrays.asList(1, -5, 3); 
         List<Integer> right = Arrays.asList(1, 2, 3);
 
-        // 2. Actuar y Verificar (Act & Assert)
-        // Verificamos que al ejecutar 'add' con estos datos, se lance IllegalArgumentException
         assertThrows(IllegalArgumentException.class, () -> {
             new addArrays().add(left, right);
         }, "El método debería lanzar IllegalArgumentException si hay dígitos negativos");
